07c1aaddd3caabb279265c080e96b71b58b751fb
Joseph Fosco <joseph@server.fake>
2013-10-06 16:33:44 -0500

    corrected :license in projuct.clj

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
 (defproject transport "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+  :license {:name "GNU General Public License version 3"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone "0.8.1"]
