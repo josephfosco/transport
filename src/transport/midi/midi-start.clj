2f4171c63041f57d44c21cd08ffc32c6c5fbcf99
Joseph Fosco <joseph@josephfosco.com>
2015-08-19 16:58:51 -0700

    capturing midi input works

--- src/transport/midi/midi-start.clj
+++ src/transport/midi/midi-start.clj
@@ -24,23 +24,59 @@
   (println "MIDI IN")
   )
 
+(def kb (midi/midi-find-device (midi/midi-sources) "VirMIDI [hw:1,0,0]"))
+
+(def keyboard (midi/midi-in kb))
+
+(midi/midi-handle-events keyboard #'midi-print)
+
+
+
+
+
+
 (first (midi-find-connected-devices "hw:1,0,0"))
 
 
-(def kb (first (midi-find-connected-devices "hw:1,0,0")))
+(def kb (midi-find-connected-device "hw:1,0,0"))
 
 (println kb)
-
-(midi/midi-in kb)
 
 kb
 
 (midi-device-num kb)
 
-(midi/midi-handle-events kb #'midi-print)
+(midi/midi-handle-events kb #(midi-print %1))
 
 (on-event [:midi :note-on] (fn [{note :note velocity :velocity}]
                              (println "Note: " note ", Velocity: " velocity))
           ::note-printer)
 
 (remove-event-handler ::note-printer)
+
+
+(def kb (midi/midi-find-device (midi/midi-sources) "VirMIDI [hw:1,0,0]"))
+
+(def kb (midi/midi-find-device (midi/midi-sources) "VirMIDI, VirMidi, Virtual Raw MIDI"))
+
+(midi/midi-find-device (midi/midi-sources) "VirMIDI, VirMidi, Virtual Raw MIDI")
+
+kb
+
+(midi/midi-in kb)
+
+(let [receiver (proxy [Receiver] [] (close [] nil) (send [msg timestamp] (#'midi-print (midi-msg msg) timestamp)))])
+
+(midi/midi-ports)
+
+(definst ding
+  [note 60 velocity 100]
+  (let [freq (midicps note)
+        snd  (sin-osc freq)
+        env  (env-gen (perc 0.1 0.8) :action FREE)]
+    (* velocity env snd)))
+
+(defn midi-player [event]
+  (ding (:note event) (/ (:velocity event) 127.0)))
+
+(midi-handle-events kb #'midi-player)
