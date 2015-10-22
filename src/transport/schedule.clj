87925f669d219a19ecf4abc5204a2d66923370a8
Joseph Fosco <joseph@josephfosco.com>
2015-10-22 08:44:50 -0700

    removed duplicate debug.clj

--- src/transport/schedule.clj
+++ src/transport/schedule.clj
@@ -15,7 +15,7 @@
 
 (ns transport.schedule
   (:require
-   [transport.debug :refer [debug-transport debug-run1]]
+   [transport.util.debug.debug :refer [debug-transport debug-run1]]
    [transport.util.count-vector :refer [count-vector]]
    [transport.util.utils :refer :all]
    ))
